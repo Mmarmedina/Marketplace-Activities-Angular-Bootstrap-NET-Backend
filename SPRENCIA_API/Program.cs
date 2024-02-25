8d66b47dd8881fc352c46a44be909464e94757c2
Mmarmedina <maria.martin@codehouse.academy>
2024-02-25 19:32:01 +0100

    Configurar program.cs y startup.cs

--- SPRENCIA_API/Program.cs
+++ SPRENCIA_API/Program.cs
@@ -1,34 +1,19 @@
 
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
 namespace SPRENCIA_API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
     }
 }
---
+++ SPRENCIA_API/Startup.cs
@@ -0,0 +1,63 @@
+﻿using SPRENCIA.CrossCutting.Configuration;
+
+namespace SPRENCIA_API
+{
+    public class Startup
+    {
+        public Startup(IHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            IoC.Register(services);
+
+            services.AddSwaggerGen(cfg =>
+            {
+                cfg.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "1.0",
+                    Title = "Pacman Gestiones de reservas restaurante"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.SwaggerEndpoint("./v1/swagger.json", "PacmanApi net core curso.Net");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+        }
+    }
+}
