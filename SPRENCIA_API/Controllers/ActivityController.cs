bf55bba9bc11e75aecb8e627d302573d2c0d54f1
Mmarmedina <maria.martin@codehouse.academy>
2024-02-25 18:43:46 +0100

    Creación controlador. Configuración petición HttpGet - Actividades

---
+++ SPRENCIA_API/Controllers/ActivityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPRENCIA.Application.Contracts.Services;
+using SPRENCIA.Domain.Models;
+
+namespace SPRENCIA_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivityController : ControllerBase
+    {
+        private readonly IActivityService _activityService;
+
+        public ActivityController(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        [HttpGet]
+        public async Task<List<Activity>> GetAll()
+        {
+            var activities = await _activityService.GetAll();
+            return activities;
+        }
+        
+    }
+}
